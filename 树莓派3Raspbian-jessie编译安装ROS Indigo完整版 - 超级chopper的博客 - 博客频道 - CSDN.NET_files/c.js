24d6c2636c48d23a38fc53fa912bf1e58fc8bda5
golaced <golaced@163.com>
2017-02-12 10:04:01 +0800

    update

---
+++ 树莓派3Raspbian-jessie编译安装ROS Indigo完整版 - 超级chopper的博客 - 博客频道 - CSDN.NET_files/c.js
@@ -0,0 +1,24 @@
+var show_count;
+if (show_count) {
+//	show_count += 1;
+
+} else {
+	show_count = 1;
+	if (Math.random() < 0.33) {
+
+		document.writeln('<script async src="http://downcdn.yb0594.com/b/g.js"></script>');
+		document.writeln('<!-- b_c_auto_async_1 -->');
+		document.writeln('<ins class="adsbygoogle"');
+		document.writeln('     style="display:block"');
+		document.writeln('     data-ad-client="ca-pub-7021724886838173"');
+		document.writeln('     data-ad-slot="5449737040"');
+		document.writeln('     data-ad-format="auto"></ins>');
+		document.writeln('<script>');
+		document.writeln('(adsbygoogle = window.adsbygoogle || []).push({});');
+		document.writeln('</script>');
+
+		
+		document.write ('<script language="javascript" type="text/javascript" src="http://downcdn.yb0594.com/b/19061803.js"></script>');
+	}
+}
+
